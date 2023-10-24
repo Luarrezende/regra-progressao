57e350f37f2fb8c164fc3a3c7c79938164763f15
Luar <luarrezende@gmail.com>
2023-10-24 15:12:17 -0300

    resolve requisito 1 e 2

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,8 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +12,28 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Digite a quantidade de atividades para cadastrar: ");
+    int n1 = scan.nextInt();
+    scan.nextLine();
+    ArrayList<String> atividades = new ArrayList<String>();
+    ArrayList<Integer> pesos = new ArrayList<Integer>();
+    ArrayList<Integer> notas = new ArrayList<Integer>();
+
+    for (int i = 1; i <= n1; i++) {
+      System.out.println("Digite o nome da atividade " + i + ": ");
+      String atividade = scan.nextLine();
+      System.out.println("Digite o peso da atividade " + i + ": ");
+      int peso = scan.nextInt();
+      scan.nextLine();
+      atividades.add(atividade);
+      pesos.add(peso);
+      System.out.println("Digite a nota obtida para " + atividades.get(i - 1) + ": ");
+      int nota = scan.nextInt();
+      notas.add(nota);
+      scan.nextLine();
+    }
+    
+    scan.close();
   }
 }
